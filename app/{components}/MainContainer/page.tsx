faaaadcff4032a86ebebfc3d8896eb7071cc1d2e
marchAlbion <marchAlbion@gmail.com>
2024-08-26 04:13:21 +0300

    fix error

--- app/{components}/MainContainer/page.tsx
+++ app/{components}/MainContainer/page.tsx
@@ -1,10 +1,6 @@
 
 
-type Props = {
-  children: React.ReactNode;
-};
-
-export default function Container({ children }: Props) {
+export default function Container({ children }: any) {
   return (
     <div className="container mx-auto px-4 sm:px-6 lg:px-8 h-[90%]">{children}</div>
   );
